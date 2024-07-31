7d751be7b922800b4b9820a41101f80916cc72b8
CaleighLowther <157258901+CaleighLowther@users.noreply.github.com>
2024-07-31 13:20:48 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+/*
+    Student Name: Caleigh Lowther
+    File Name: script.js
+    Date: July 30th 2024 
+*/ 
+//Global variables
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
+
+//ans1
+function ans1{
+	//code
+}
+//ans2
+function ans2{
+	//code
+}
+//ans3
+function ans3{
+	//code
+}
+//ans4
+function ans4{
+	//code
+}
+
+//Hamburrger menu function
+function menu{
+	//code
+}
+//Function to display the first picture
+function pic1 {
+    imgSource.src = "images/trunk-bay.jpg"
+    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block
+    figCap.textContent = "Trunk Bay in St. John";
